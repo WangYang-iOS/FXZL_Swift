ca55b3cb47000768bd87e741033d9322b48dac74
wangyang <wangyang@konggeek.com>
2019-03-13 22:02:26 +0800

    人脉详情

---
+++ FXZL_Swift/FXZL_Swift/Classes/ViewController/DetailVC/HQConnectDetailVC.swift
@@ -0,0 +1,16 @@
+//
+//  HQConnectDetailVC.swift
+//  FXZL_Swift
+//
+//  Created by WY的七色花 on 2019/3/13.
+//  Copyright © 2019 yons. All rights reserved.
+//
+
+import UIKit
+
+class HQConnectDetailVC: HQBaseVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
